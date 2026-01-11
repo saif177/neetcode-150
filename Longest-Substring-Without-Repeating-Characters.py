f241a7a73fc23ef9e8b1623a7c43b6e3db7a2f44
saif177 <saif.blt17@gmail.com>
2026-01-11 23:35:24 +0530

    solved

---
+++ Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,14 @@
+def longestSubStr(s):
+    res = 0
+    charset = set()
+    left = 0
+    for right in range(len(s)):
+        while s[right] in charset:
+            charset.remove(s[left])
+            left += 1
+        charset.add(s[right])
+        res = max(res, right - left + 1)
+    return res
+
+s = "zxyzxyz"
+print(longestSubStr(s))
