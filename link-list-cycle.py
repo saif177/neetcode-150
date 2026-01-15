48fec7701410c29f307f6fb6fc85a674902e2d52
saif177 <saif.blt17@gmail.com>
2026-01-15 23:27:20 +0530

    link list

---
+++ link-list-cycle.py
@@ -0,0 +1,32 @@
+def hasCycle(head) -> bool:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node3
+
+print(hasCycle(node1))
